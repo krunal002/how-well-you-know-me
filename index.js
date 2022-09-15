492d2a5e334c910bbc98d9404e93f946ea444c14
krunal002 <>
2022-09-15 05:30:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+let read = require("readline-sync");
+
+//welcome note
+console.log("How Well You Know Me!")
+let user = read.question("Enter your user name - ");
+console.log("welcome " + user + " to the K-code\n");
+
+//questions for marvel comic
+//array for question using objects
+let que1 = [
+  {
+    Q: "\n1. Which is my favorite drink? \n 1.maza\n 2.coco-cola\n 3.sprite\n 4.limka\n \n--> ",
+    A: "maza"
+  },
+  {
+    Q: "2. where I studied? \n 1.jvv\n 2.ashok\n 3.nutan\n 4.aadarsh\n \n--> ",
+    A: "jvv"
+  },
+  {
+    Q: "3. Who is my favorite? \n 1.salman\n 2.tiger\n 3.hritik\n 4.srk\n \n--> ",
+    A: "hritik"
+  },
+  {
+    Q: "4. The mobile I have- \n 1.iphone\n 2.samsung\n 3.xiaomi\n 4.nokia\n \n--> ",
+    A: "xiaomi"
+  },
+  {
+    Q: "5. Where I prefer to go? \n 1.hills\n 2.lake\n 3.farm\n 4.forest\n \n--> ",
+    A: "hills"
+  }
+]
+
+let score = 0;
+  for (let i = 0; i < que1.length; i++) {
+    let CQ = que1[i];
+    game(CQ.Q, CQ.A);
+  }
+
+
+
+//function
+function game(question, answer) {
+  let u_ans = read.question(question);
+  if (u_ans === answer) {
+    console.log("Right");
+    score++;
+  }
+  else
+    console.log("Wrong");
+
+  console.log("current score is " + score);
+  console.log("-------------------------------\n");
+}
+
+console.log("Thank You for playing " + user);
+console.log("Your final score is " + score + "/5");
